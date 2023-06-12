958655e7ce1567105c651616e6f9db129115c85e
KChudzicki <chudzicki.krzysztof1@gmail.com>
2023-06-12 21:15:11 +0200

    Initial commit

---
+++ src/main/java/com/myspellbook/MySpellBookApplication.java
@@ -0,0 +1,13 @@
+package com.myspellbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpellbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpellbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myspellbook/MySpellBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspellbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpellbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
