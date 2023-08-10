b23b507a251ae0fe927cca0c10a697f265a647d2
KChudzicki <chudzicki.krzysztof1@gmail.com>
2023-08-10 18:53:52 +0200

    Szkielet strony z czarami maga

--- src/main/java/com/myspellbook/web/controllers/WizzardSpellPage.java
+++ src/main/java/com/myspellbook/web/controllers/WizzardSpellPage.java
@@ -3,14 +3,12 @@ package com.myspellbook.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomePageController {
+public class WizzardSpellPage {
 
-
-    @GetMapping("/")
-    public String homePage(Model model) {
-      return "homePage";
+    @GetMapping("/wizard")
+    public String WizzardSpellPage(Model model) {
+        return "wizard";
     }
 }
